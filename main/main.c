708c4f79e229aa7431caebc9da784a6d557c1b1f
bignibba01 <davidemarchetti01@gmail.com>
2021-11-23 17:46:26 +0100

    Aggiungere i file di progetto.

---
+++ main/main.c
@@ -0,0 +1,15 @@
+/*
+Realizzare un programma che gestisca la lista di tutti i libri letti in un anno. Per ognuno di essi, il programma deve memorizzare una serie di informazioni,
+ad esempio il titolo, l’autore, l’anno di pubblicazione, la casa editrice, la lunghezza, il genere, il codice identificativo, la valutazione.
+Il programma dovrà permettere di inserire un nuovo libro, cancellarne uno, visualizzarli tutti, visualizzare solo quelli con una certa valutazione.
+Il programma deve lavorare leggendo e scrivendo le informazioni da e su file binario. Consegnare un file .zip contente un progetto che comprenda tutti
+i file necessari per testare il programma.
+
+VENTRUCCI TOMAS, MARCHETTI DAVIDE, ZOLI FEREDEICO
+*/
+
+#include <iostream>
+
+int main() {
+
+}
